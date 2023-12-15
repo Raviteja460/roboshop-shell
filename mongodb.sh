d8a3035249640ea1c343ecfd0010dbd8add25e08
Raviteja460 <seelamsettyraviteja@gmail.com>
2023-12-15 19:59:31 +0530

    Roboshop-shellscript

--- mongodb.sh
+++ mongodb.sh
@@ -33,7 +33,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "Copied MongoDB Repo"
 
-dnf install mongodb -y &>> $LOGFILE
+dnf install mongodb-org -y &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB"
 
